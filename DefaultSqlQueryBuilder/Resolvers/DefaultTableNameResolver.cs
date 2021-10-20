aecf4fd50f0bb483e7c0346263237421de988c30
Chee Kian Hong <kianhong.chee@infradigital.com.my>
2021-10-20 12:52:40 +0800

    - Added custom table name attribute

--- DefaultSqlQueryBuilder/Resolvers/DefaultTableNameResolver.cs
+++ DefaultSqlQueryBuilder/Resolvers/DefaultTableNameResolver.cs
@@ -2,13 +2,20 @@ using System;
 
 namespace DefaultSqlQueryBuilder.Resolvers
 {
-	public class DefaultTableNameResolver : ITableNameResolver
-	{
-		public string Resolve(Type type)
-		{
-			if (type == null) throw new ArgumentNullException(nameof(type));
+    public class DefaultTableNameResolver : ITableNameResolver
+    {
+        public string Resolve(Type type)
+        {
+            if (type == null) throw new ArgumentNullException(nameof(type));
 
-			return $"[{type.Name}]";
-		}
-	}
+            var customAttribute = type.GetCustomAttributes(typeof(TableNameAttribute), true);
+            if (customAttribute.Length > 0)
+            {
+                var tableNameAttribute = customAttribute[0] as TableNameAttribute;
+                return $"[{tableNameAttribute.TableName}]";
+            }
+
+            return $"[{type.Name}]";
+        }
+    }
 }
---
+++ DefaultSqlQueryBuilder/TableNameAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DefaultSqlQueryBuilder
+{
+    /// <summary>
+    /// Database Table Name
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class TableNameAttribute : Attribute
+    {
+        public TableNameAttribute(string tableName)
+        {
+            TableName = tableName;
+        }
+
+        public string TableName { get; set; }
+    }
+}
